bf44dd1938c510d9766f56b4ccd4e8e533fb85da
Eremeev Andery <a.yeremyeyev@gmail.com>
2020-01-20 21:55:50 +0200

    3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 // _________--------------------_____
 // function coffeMachine(coffee, callBack) {
 //   setTimeout(function() {
-//     coffee += m;  
+//     coffee += m;
 //     return callBack(coffee);
 //   }, 1000);
 // }
@@ -25,31 +25,53 @@
 // });
 //-----------------------
 //const fs = require("fs");
-const request = require('request');
+
+const request = require("request");
 const http = require("http");
 const port = 3000;
 let api = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
 // let data = fs.readFile("index.html");
-http
-  .createServer(function(request, response) {
-    if (req.url === "/courses") {
-      res.write(api);
-      res.end();
-    } else if (req.url === "/about") {
-      console.log(req.url);
-      res.write("we are from here!");
-      res.end();
-    } 
-  })
-  .listen(port, function() {
-    console.log("Server at http://localhost:3000");
-  });
 
-
-    let any = func('localhost:3000/courses').pipe(request.put(api));
-
-    return callBack(api);
+// function piska (){ http
+//   .createServer(function func(request, response, callBack) {
+//     if (req.url === "/courses") {
+//       res.write(api);
+//       res.end();
+//       return callBack;
+//     } else (req.url === "/about");
+//       console.log(req.url);
   
-    https://metanit.com/web/nodejs/3.1.php
-    https://www.npmjs.com/package/request
-    https://www.npmjs.com/package/http
+//   })
+//   .listen(port, function() {
+//     console.log("Server at http://localhost:3000/courses");
+//   });
+// }
+// piska(
+  // request.get("localhost:3000/courses").pipe(request.put(api)));
+  // return callBack(api);
+
+
+//request.get('localhost:3000/courses').pipe(request.put('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'))
+
+// https://metanit.com/web/nodejs/3.1.php
+// https://www.npmjs.com/package/request
+// https://www.npmjs.com/package/http
+
+
+function request111(coffee, cb){
+ 
+http.createServer(function (req, resp) {
+  if (req.url === '/courses') {
+    r.get(cb).pipe(resp)
+  }
+}).listen(port, function() {
+    console.log("Server at http://localhost:3000/courses");
+     });
+}
+
+let coffee = "black";
+let data = 1000;
+request111(function (coffee,data){
+       console.log(data);
+  
+   });
